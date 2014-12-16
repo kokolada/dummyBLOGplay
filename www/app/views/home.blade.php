647cfa98add49d409bafe26ab22102a5143773c7
kokolada <aanicic183@gmail.com>
2014-12-16 21:23:12 +0100

    home

---
+++ www/app/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.master')
+
+@section('content')
+
+<div>
+	<img src="" alt="">
+	<h1>Ja predstavljam banner 1</h1>
+</div>
+
+<div>
+	<section name="slideshow vijesti">
+		<img src="">
+	</section>
+	<section name="3 reklame">
+		<img src="" alt="reklama1">
+		<img src="" alt="reklama2">
+		<img src="" alt="reklama3">
+	</section>
+	<section name="banner i uclani se">
+		<h2>Ja predstavljam banner 2</h2>
+		<div>uclani se</div>
+	</section>
+	<section name="vijesti">
+		<h3>Naslov za prvu vijest</h3>
+		<img src="" alt="slikazavijest">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex ullam tempora dignissimos deserunt blanditiis, corporis tempore harum eos officia error sed repellendus quod, dolor cumque doloremque velit. Minus quos quis incidunt, earum nobis impedit voluptates inventore consectetur voluptatem blanditiis, animi itaque nam nisi temporibus nostrum cupiditate cumque omnis ab hic.</p>
+	</section>
+	<section name="pdf i face">
+		<div>pdf</div>
+		<img src="" alt="face">
+	</section>
+</div>
+
+@stop
---
+++ www/app/views/layout/navbar.blade.php
@@ -0,0 +1,9 @@
+<img src="" alt="slika">
+<h1>CDOM</h1>
+<ul>
+	<li>O CDOM CARD</li>
+	<li>UČLANI SE</li>
+	<li>PARTNERI</li>
+	<li>CDOM</li>
+	<li>KONTAKT</li>
+</ul>
